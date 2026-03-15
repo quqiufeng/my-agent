3d953c02f6f290d07321a02185f1f6d1e6199c8b
quqiufeng <quqiufeng@163.com>
2026-03-15 13:12:30 +0800

    feat: 支持 !quit 退出程序

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
             if not user_input:
                 continue
 
-            if user_input.lower() in ["quit", "exit", "q"]:
+            if user_input.lower() in ["quit", "exit", "q", "!quit"]:
                 print("再见!")
                 break
 
